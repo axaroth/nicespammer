4f8be0e7337386a6540a8796d61989626dd8a8ed
riccardo.lemmi <riccardo.lemmi@9ba5466c-fc75-11de-9a2d-79d510f670ec>
2010-01-27 17:35:19 +0000

    added -c option: treats the path as a container of newsletters

--- trunk/mailer_sender.buildout/src/nicespammer.mailgenerator/nicespammer/mailgenerator/generate.py
+++ trunk/mailer_sender.buildout/src/nicespammer.mailgenerator/nicespammer/mailgenerator/generate.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
-import sys, os, shutil
+import sys
+import os
+import shutil
 from optparse import  OptionParser
 
 from nicespammer.mailgenerator import MailGenerator
@@ -9,6 +11,17 @@ def empty_mails(src):
     f = open(src, 'w')
     f.write('mail\n')
     f.close()
+
+def listdirs(src_folder):
+    paths = []
+    for (dirpath, dirnames, filenames) in os.walk(src_folder):
+        for name in dirnames:
+            path = os.path.join(src_folder, name)
+            conf = os.path.join(path, 'newsletter.cfg')
+            if os.path.exists(conf):
+                paths.append(path)
+    return paths
+
 
 class CommandGenerator(object):
     """ """
@@ -39,6 +52,13 @@ class CommandGenerator(object):
                   dest="force",
                   default=False,
                   help="Generate emails now")
+        parser.add_option(
+                  "-c",
+                  "--container",
+                  action="store_true",
+                  dest="isContainer",
+                  default=False,
+                  help="The path is to a container of newsletter configurations")
         (options, args) = parser.parse_args()
 
         if  len(args) != 1:
@@ -51,30 +71,39 @@ class CommandGenerator(object):
                 parser.error("Invalid email address.")
                 sys.exit(-1)
 
-        newsletter_path = args[0]
-        mg = MailGenerator(newsletter_path)
-
-        if options.address is not None:
-            mg.generate_single_mail(options.address)
-        elif options.force:
-            mg.generate()
+        path = args[0]
+        if options.isContainer == True:
+            newsletter_paths = listdirs(path)
         else:
-            mg.check_and_generate()
+            newsletter_paths = [path, ]
 
-        if options.purge:
-            src = mg.csv_file_path
-            if os.path.exists(src):
-                i = 0
-                dst = src+'.%i'%i
-                while os.path.exists(dst):
-                    i += 1
+        for newsletter_path in newsletter_paths:
+
+            print "Go for: ", newsletter_path
+
+            mg = MailGenerator(newsletter_path)
+
+            if options.address is not None:
+                mg.generate_single_mail(options.address)
+            elif options.force:
+                mg.generate()
+            else:
+                mg.check_and_generate()
+
+            if options.purge:
+                src = mg.csv_file_path
+                if os.path.exists(src):
+                    i = 0
                     dst = src+'.%i'%i
-                    if i > 100:
-                        raise Exception('Reached max number of backup files')
+                    while os.path.exists(dst):
+                        i += 1
+                        dst = src+'.%i'%i
+                        if i > 100:
+                            raise Exception('Reached max number of backup files')
 
-                shutil.move(src, dst)
-                empty_mails(src)
-            print 'Removed emails'
+                    shutil.move(src, dst)
+                    empty_mails(src)
+                print 'Removed emails'
 
         return 0
 
