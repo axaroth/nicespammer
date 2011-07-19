ebc0382ed855af5e6d76e8838bc125ea699d5645
riccardo.lemmi <riccardo.lemmi@9ba5466c-fc75-11de-9a2d-79d510f670ec>
2011-07-19 10:51:47 +0000

    (1.0) refactoring tree

---
+++ nicespammer.buildout/tags/1.0/src/nicespammer.statistics/nicespammer/statistics/db_setup.py
@@ -0,0 +1,36 @@
+import sys
+from pysqlite2 import dbapi2 as sqlite3
+
+def creation(path='./example.db'):
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+
+    # Table Emails
+    c.execute('''
+        create table emails
+        (email_id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT)''')
+
+    # Table Newsletters
+    c.execute('''
+        create table newsletters
+        (newsletter_id INTEGER PRIMARY KEY AUTOINCREMENT, newsletter_name TEXT)''')
+
+    # Table Bindings
+    c.execute('''
+        create table bindings
+        (newsletter_id INTEGER, email_id INTEGER)''')
+
+    # Table Clicks: newsletter-id mail-id date
+    c.execute('''
+        create table clicks
+        (newsletter_id INTEGER, email_id INTEGER, date TEXT)''')
+
+    conn.commit()
+    c.close()
+
+
+def main():
+    creation(sys.argv[1])
+    
+if __name__=='__main__':
+    main()
